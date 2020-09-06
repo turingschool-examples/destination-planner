79656b4e6dd8a908cf1df5be9d101682abc0cd73
Phillip Strom <phillip.strom@gmail.com>
2020-09-06 16:08:21 -0600

    Create forecast poro

---
+++ app/poros/forecast.rb
@@ -0,0 +1,22 @@
+require 'date'
+
+class Forecast
+  attr_reader :summary, :temp, :temp_min, :temp_max, :date
+
+  def initialize(info)
+    @temp = convert_k_to_f_temp(info[:main][:temp])
+    @temp_min = convert_k_to_f_temp(info[:main][:temp_min])
+    @temp_max = convert_k_to_f_temp(info[:main][:temp_max])
+    @summary = info[:weather][0][:description]
+    @date = DateTime.now.new_offset(info[:timezone])
+  end
+
+  def formatted_date
+    date.strftime('%A, %B%e')
+  end
+
+  def convert_k_to_f_temp(kelvin)
+    temp = ((kelvin - 273.15) * 9/5 + 32).round
+    temp.to_s + '°F'
+  end
+end
---
+++ spec/poros/forecast_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe Forecast do
+  before :each do
+    @info = {
+      "coord": {
+          "lon": -104.9,
+          "lat": 39.58
+      },
+      "weather": [
+          {
+              "id": 500,
+              "main": "Rain",
+              "description": "sunny",
+              "icon": "10d"
+          }
+      ],
+      "base": "stations",
+      "main": {
+          "temp": 305.37,
+          "feels_like": 303.37,
+          "temp_min": 291.483,
+          "temp_max": 308.15,
+          "pressure": 1023,
+          "humidity": 7
+      },
+      "visibility": 10000,
+      "wind": {
+          "speed": 2.1,
+          "deg": 0
+      },
+      "rain": {
+          "1h": 0.51
+      },
+      "clouds": {
+          "all": 20
+      },
+      "dt": 1599424446,
+      "sys": {
+          "type": 1,
+          "id": 3292,
+          "country": "US",
+          "sunrise": 1599395587,
+          "sunset": 1599441767
+      },
+      "timezone": -21600,
+      "id": 0,
+      "name": "Englewood",
+      "cod": 200
+    }
+    @forecast = Forecast.new(@info)
+  end
+  it 'can be initialized with attributes' do
+    date = DateTime.now.new_offset(@info[:timezone])
+
+    expect(@forecast).to be_instance_of(Forecast)
+    expect(@forecast.temp).to eq('90°F')
+    expect(@forecast.temp_max).to eq('95°F')
+    expect(@forecast.temp_min).to eq('65°F')
+    expect(@forecast.date).to be_instance_of(DateTime)
+    expect(@forecast.date.offset).to eq(date.offset)
+    expect(@forecast.summary).to eq('sunny')
+  end
+   it "can format date" do
+   date = DateTime.now.new_offset(@info[:timezone])
+   expected = date.strftime('%A, %B%e')
+   expect(@forecast.formatted_date).to eq(expected)
+  end
+  it "can convert temperatures" do
+    expect(@forecast.temp).to eq('90°F')
+    expect(@forecast.temp_max).to eq('95°F')
+    expect(@forecast.temp_min).to eq('65°F')
+  end
+end
