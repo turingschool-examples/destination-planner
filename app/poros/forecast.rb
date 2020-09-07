896bfa9af7fff375565a17887620a934ba8a2497
Phillip Strom <phillip.strom@gmail.com>
2020-09-06 19:01:08 -0600

    Add image url

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,14 +1,15 @@
 require 'date'
 
 class Forecast
-  attr_reader :summary, :temp, :temp_min, :temp_max, :date
+  attr_reader :summary, :temp, :temp_min, :temp_max, :date, :image_url
 
-  def initialize(info)
+  def initialize(info, gif)
     @temp = convert_k_to_f_temp(info[:main][:temp])
     @temp_min = convert_k_to_f_temp(info[:main][:temp_min])
     @temp_max = convert_k_to_f_temp(info[:main][:temp_max])
     @summary = info[:weather][0][:description]
     @date = DateTime.now.new_offset(info[:timezone])
+    @image_url = gif
   end
 
   def formatted_date
