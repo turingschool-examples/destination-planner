3d94704d03352a0e828ceac1aec4c6e2eb861804
Phillip Strom <phillip.strom@gmail.com>
2020-09-06 19:04:03 -0600

    Refactor get_gif

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,8 +1,14 @@
 class ForecastFacade
   def self.weather_by_zip(zip)
     info = WeatherService.by_zip(zip)
-    gif_info = GifService.image_by_description(info[:weather][0][:description])
-    gif = gif_info[:images][:fixed_height][:url]
+    gif = get_gif(info[:weather][0][:description])
     Forecast.new(info, gif)
+  end
+
+  private
+
+  def self.get_gif(weather_summary)
+    gif_info = GifService.image_by_description(weather_summary)
+    gif_info[:images][:fixed_height][:url]
   end
 end
