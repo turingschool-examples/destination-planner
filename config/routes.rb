1be6f699918ea571401fdbd733bdcadf142799d1
Phillip Strom <phillip.strom@gmail.com>
2020-09-07 15:27:01 -0600

    Add update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :destinations
   namespace :api do
     namespace :v1 do
-      resources :destinations, only: [:index, :show, :create]
+      resources :destinations, only: [:index, :show, :create, :update]
     end
   end
 end
