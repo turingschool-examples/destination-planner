0a36721187736d937187d6ea1bc3b7570f5b8bf5
Phillip Strom <phillip.strom@gmail.com>
2020-09-07 15:02:04 -0600

    Create show action

--- app/channels/api/v1/destinations_controller.rb
+++ app/channels/api/v1/destinations_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::DestinationsController < ApplicationController
   def index
     render json: DestinationSerializer.render(Destination.all)
   end
+
+  def show
+    render json: DestinationSerializer.render(Destination.find(params[:id]))
+  end
 end
