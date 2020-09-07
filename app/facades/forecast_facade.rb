7f6e5d79209bba246e534882c923469880a905bc
Phillip Strom <phillip.strom@gmail.com>
2020-09-06 19:01:32 -0600

    Add gif methods to facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,8 @@
 class ForecastFacade
   def self.weather_by_zip(zip)
     info = WeatherService.by_zip(zip)
-    Forecast.new(info)
+    gif_info = GifService.image_by_description(info[:weather][0][:description])
+    gif = gif_info[:images][:fixed_height][:url]
+    Forecast.new(info, gif)
   end
 end
