136a264c4ba4f00934b7bcb0985d4c73a060b31f
Phillip Strom <phillip.strom@gmail.com>
2020-09-07 14:49:40 -0600

    Create api destinations spec

---
+++ spec/requests/api/v1/destinations/request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "Destination API" do
+  before :each do
+    @destination1 = Destination.create!(
+      name: 'Centennial',
+      zip: 80112,
+      description: "Yup. it's Suburbia!",
+      image_url: Faker::Placeholdit.image
+    )
+    @destination2 = Destination.create!(
+      name: 'Denver',
+      zip: 80210,
+      description: "No, it's *8* 0210",
+      image_url: Faker::Placeholdit.image
+    )
+  end
+  it "sends a list of destinations" do
+    get '/api/v1/destinations'
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    destinations = parsed[:data]
+
+    expect(response).to be_successful
+    expect(response.content_type).to eq('application/json')
+    expect(destinations.count).to eq(2)
+    destinations.each do |destination|
+      expect(destination[:type]).to eq('destination')
+    end
+    expect(destinations.first[:id]).to eq(@destination1.id)
+    expect(destinations.first[:attributes][:name]).to eq(@destination1.name)
+    expect(destinations.first[:attributes][:zip]).to eq(@destination1.zip)
+    expect(destinations.first[:attributes][:description]).to eq(@destination1.description)
+  end
+end
